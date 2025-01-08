23581757f50296eb3ac51d392e8fcdb980822f47
serlyfitriaulia <fitriserli667@gmail.com>
2025-01-08 16:17:29 +0700

    halaman

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -69,13 +69,6 @@
                                                 <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">Data Genre</div>
                                                 </a>
                                             </div>
-                                            <div class="col">
-                                                <div class="progress progress-sm mr-2">
-                                                    <div class="progress-bar bg-info" role="progressbar"
-                                                        style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                                        aria-valuemax="100"></div>
-                                                </div>
-                                            </div>
                                         </div>
                                     </div>
                                     <div class="col-auto">
@@ -165,24 +158,6 @@
                     </div>
                 </div>
 
-                <div class="col-xl-3 col-md-6 mb-4">
-                    <div class="card border-left-primary shadow h-100 py-2">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                        Film</div>
-                                     <div class="col-auto">
-                                            <a href="{{ url('film',[])}}">Data Film
-                                            <i class="bi bi-people-fill text-blue-700 fa-2x"></i>
-                                            </a>
-                                     </div>
-                                </div>
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
                 <div class="col-xl-3 col-md-6 mb-4">
                     <div class="card border-left-warning shadow h-100 py-2">
